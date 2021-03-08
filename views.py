347ca8281fcccc61f6a057d92197fabea8546604
Chris Walquist <cwalquist@drw.com>
2021-03-08 16:49:12 -0600

    Round 2: A better DELETE route. Use jQuery to intercept click event on <a> element with 'delete_link' class, and send a DELETE request

--- views.py
+++ views.py
@@ -3,13 +3,27 @@ from datetime import datetime
 from app import app
 from tag import Tag
 
+delete_js = '''
+<script type="text/javascript" src="static/js/lib/jquery-3.5.1.js"></script>
+<script type="text/javascript">
+$('.delete_link').on('click', function(e) {
+  var url = "http://localhost:5000" + $(this).attr('href');
+  var xhr = new XMLHttpRequest();
+  xhr.open("DELETE", url, true);
+  xhr.send(null);
+  window.location.href='/';  // Redirect
+  return false;
+});
+</script>
+'''
+
 @app.route('/', methods=['GET'])
 def show_tags():
     tags = Tag.select()
-    tags_html = '\n'.join(list(map(lambda x: "<a href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
+    tags_html = '\n'.join(list(map(lambda x: "<a class=\"delete_link\" href=\"/tags/%s\">%s</a><br>" % (x.name, x.name), tags)))
     form_html = "<form action=\"/tags\" method=\"POST\"><label>Enter a tag: </label><input name=\"tag-name\"></form>"
     #embed()
-    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>" % (app.config['config']['awesome_image'],tags_html, form_html)
+    return "<h1>The Ultimate Tag Manager</h1><h1>Hello World!</h1><img src=\"%s\" style=\"width:300px\"><div>%s</div><div>%s</div>%s" % (app.config['config']['awesome_image'],tags_html, form_html,delete_js)
 
 @app.route('/tags', methods=['POST'])
 def add_tag():
@@ -19,9 +33,8 @@ def add_tag():
 
     return redirect('/')
 
-# GET is not the recommended way to implement DELETE, but oh well...
-@app.route('/tags/<tag>', methods=['GET'])
+@app.route('/tags/<tag>', methods=['DELETE'])
 def remove_tag(tag):
     tag_to_remove = Tag.get(Tag.name == tag).delete_instance()
 
-    return redirect('/')
+    return '', 204 # No need to return any content, since Javascript will redirect anyway
